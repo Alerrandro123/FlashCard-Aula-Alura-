2921ce9142403e17ba25aa37e7a932708395e478
Alerrandro123 <44883066+Alerrandro123@users.noreply.github.com>
2024-10-23 16:27:18 -0300

    Projeto Final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Inglês',
+    'Como é qual o seu nome em inglês?',
+    'Qual o seu nome em inglês é "Whats your name?".'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a quantidade de continentes (De acordo com o ensino brasileiro)?',
+    'Existe um total de 6 continentes, sendo eles: África, América, Antártida, Ásia, Europa e Oceania.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Front-End?',
+    'O front-end é o que usuários veem em seus sites e inclui elementos visuais, como botões, caixas de seleção, gráficos e mensagens de texto.'
+)
+
+criaCartao(
+    'Histórias',
+    'Quando o Brasil ficou independente?',
+    'O Brasil ficou independente em 7 de Setembro de 1822'
+)
